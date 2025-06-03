b2a6913ef909d6770691e57f0b2e2abda40495f7
leandro <lleann445@gmail.com>
2025-06-03 20:51:36 -0300

    Update bmi.c
    se añadio posicion del usuario en la tabla

--- bmi.c
+++ bmi.c
@@ -6,35 +6,67 @@ int main()
     
     ///////////altura
     printf("Ingresa tu altura en metros: ");
-    scanf("%f", &altura);
+    scanf(" %f", &altura);
     
     ///////////peso
     printf("Ingresa tu peso en kilogramos: ");
-    scanf("%f", &peso);
+    scanf(" %f", &peso);
     
     ///////////calculo imc
     imc=peso/(altura*altura);
     printf("Tu IMC es: %.2f\n", imc);
     
-    //////////clasificacion
+    //////////clasificacion y posicionamiento en tabla
     if (imc < 18.5) {
         printf("Estado: Bajo peso\n");
+        printf("///////////////////////////////\n");
+        printf("//       Indice| Condicion   //\n");
+        printf("//---------------------------//\n");
+        printf("//       <18.5 | Bajo peso   //<--Estas aqui\n");
+        printf("// 18.5 a 24.9 | Normal      //\n");
+        printf("// 25.0 a 29.9 | Sobrepeso   //\n");
+        printf("//         >=30| Obesidad    //\n");
+        printf("///////////////////////////////");
     } else if (imc >= 18.5 && imc < 24.9) {
         printf("Estado: Normal\n");
+        printf("///////////////////////////////\n");
+        printf("//       Indice| Condicion   //\n");
+        printf("//---------------------------//\n");
+        printf("//       <18.5 | Bajo peso   //\n");
+        printf("// 18.5 a 24.9 | Normal      //<--Estas aqui\n");
+        printf("// 25.0 a 29.9 | Sobrepeso   //\n");
+        printf("//         >=30| Obesidad    //\n");
+        printf("///////////////////////////////");
     } else if (imc >= 25 && imc < 29.9) {
         printf("Estado: Sobrepeso\n");
+        printf("///////////////////////////////\n");
+        printf("//       Indice| Condicion   //\n");
+        printf("//---------------------------//\n");
+        printf("//       <18.5 | Bajo peso   //\n");
+        printf("// 18.5 a 24.9 | Normal      //\n");
+        printf("// 25.0 a 29.9 | Sobrepeso   //<--Estas aqui\n");
+        printf("//         >=30| Obesidad    //\n");
+        printf("///////////////////////////////");
     } else {
         printf("Estado: Sin miedo al segundo plato en casa ajena (obesidad)\n");
+        printf("///////////////////////////////\n");
+        printf("//       Indice| Condicion   //\n");
+        printf("//---------------------------//\n");
+        printf("//       <18.5 | Bajo peso   //\n");
+        printf("// 18.5 a 24.9 | Normal      //\n");
+        printf("// 25.0 a 29.9 | Sobrepeso   //\n");
+        printf("//         >=30| Obesidad    //<--Estas aqui\n");
+        printf("///////////////////////////////");
     }
 
-///////////////////////////////
-//Índice| Condición          //
-//---------------------------//
-//<18.5 | Bajo peso          //
-//18.5 a 24.9 | Normal       //
-//25.0 a 29.9 | Sobrepeso    //
-//>=30| Obesidad             //
-///////////////////////////////
+//printf("///////////////////////////////\n");
+//printf("//       Indice| Condicion   //\n");
+//printf("//---------------------------//\n");
+//printf("//       <18.5 | Bajo peso   //\n");
+//printf("// 18.5 a 24.9 | Normal      //\n");
+//printf("// 25.0 a 29.9 | Sobrepeso   //\n");
+//printf("//         >=30| Obesidad    //\n");
+//printf("///////////////////////////////");
     
     return 0;
 }
